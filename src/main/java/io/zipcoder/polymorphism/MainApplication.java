9523e4d289f3f1fa31fb3021b3d16e83e563b8c1
Carolina Diazgranados <linacello@gmail.com>
2022-11-25 22:36:45 -0500

    improved by adding a createPet method which has been tested

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -2,50 +2,46 @@ package io.zipcoder.polymorphism;
 
 
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Welcome to Talking Pets. \nThere are only three kinds of pets in this world. cat, dog, and bird.\n");
+        System.out.println("Welcome to Talking Pets. \nThere are only three kinds of pets in this world: cat, dog, and bird.\n");
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("How many pets do you have?");
-
         int petAmount = scanner.nextInt();
-        String[] pets = new String[petAmount];
-        int i = 0;
-        String petNum = "";
-        Pet[] petHouse = new Pet[petAmount];
+        scanner.nextLine();
 
-        while (i < petAmount) {
+        ArrayList<Pet> petList= new ArrayList<Pet>();
 
-        petNum = String.valueOf(i + 1);
-        Scanner scan = new Scanner(System.in);
-        System.out.println("What kind of pet is pet number " + petNum + "?");
-        String petType = scan.nextLine();
-        pets[i] = petType;
-            if(pets[i].equals("cat")) {
-                Cat cat =  new Cat();
-                petHouse[i] = cat;
-            } else if (pets[i].equals("dog")) {
-                Dog dog =  new Dog();
-                petHouse[i] = dog;
-            } else if (pets[i].equals("bird")) {
-                Bird bird =  new Bird();
-                petHouse[i] = bird;
-            }
-            Scanner scanName = new Scanner(System.in);
+
+        for(int i = 0; i < petAmount; i++) {
+            String petNum = String.valueOf(i + 1);
+            System.out.println("What kind of pet is pet number " + petNum + "?");
+            String petType = scanner.nextLine();
             System.out.println("What is your pets name?");
-            String petName = scanName.nextLine();
+            String petName = scanner.nextLine();
 
-            petHouse[i].setName(petName);
-            i++;
+            petList.add(createPet(petType, petName));
         }
 
-        for (int j = 0; j < pets.length; j++) {
-            System.out.println(petHouse[j].getName() + " says " + petHouse[j].speak());
+        for (int j = 0; j < petList.size(); j++) {
+            System.out.println(petList.get(j).getName() + " says " + petList.get(j).speak());
         }
 
+    }
+
+    public static Pet createPet(String pet, String petName) {
+        if(pet.equalsIgnoreCase("cat")) {
+            return new Cat(petName);
+        } else if (pet.equalsIgnoreCase("dog")) {
+            return new Dog(petName);
+        } else if (pet.equalsIgnoreCase("bird")) {
+           return new Bird(petName);
+        }
+        return null;
     }
 
 }
--- src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
+++ src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
@@ -1,10 +1,26 @@
 package io.zipcoder.polymorphism;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MainApplicationTest {
-@Test
-    public void testMe(){
+    @Test
+    public void createPetTest_Name(){
+        MainApplication main = new MainApplication();
+        Pet create = main.createPet("cat", "Red");
+        String expected = "Red";
+        String actual = create.getName();
 
-}
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void createPetTest_Speak(){
+        MainApplication main = new MainApplication();
+        Pet create = main.createPet("cat", "Red");
+        String expected = "Meow!";
+        String actual = create.speak();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
